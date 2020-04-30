e378e8957e66d5d0514812cae26ed6b64bba3838
Javier Ramirez <35494248+JavierRamirezCospin@users.noreply.github.com>
2020-04-29 23:37:19 -0500

    Second commit
    Added Admin View and polished login

--- proyectoBaseDatos.py
+++ proyectoBaseDatos.py
@@ -3,6 +3,14 @@ from tkinter import font
 import psycopg2
 import random
 
+##################################################################################################################
+                                        #Funciones para el sistema
+##################################################################################################################
+def logout(View,currentUser):
+    View.quit()
+    currentUser = {}
+    loginView()
+
 def registerUser(con):
     cur = con.cursor()
     try:
@@ -14,39 +22,75 @@ def registerUser(con):
         for cid in CID:
             lastCID = cid[0]
 
-        lastCID += 1
+        lastCID = lastCID + 1
+        lastCID = int(lastCID)
         supportRepId = random.randint(3,5)
+        supportRepId = int(supportRepId)
         rol = 1
+        rol = int(rol)
         Username = newuserEntry.get()
+        Username += ''
         password = newpasswordEntry.get()
+        password += ''
         FirstName = newFirstNameEntry.get()
+        FirstName += ''
+        State = newStateEntry.get()
+        State += ''
+        Fax = newFaxEntry.get()
+        Fax += ''
+        Company = newCompanyEntry.get()
+        Company += ''
         LastName = newLastNameEntry.get()
+        LastName += ''
         Address = newAddressEntry.get()
+        Address += ''
         City = newCityEntry.get()
+        City += ''
         Country = newCountryEntry.get()
+        Country += ''
         PostalCode = newPostalEntry.get()
+        PostalCode += ''
         Phone = newPhoneEntry.get()
+        Phone += ''
         Email = newEmailEntry.get()
+        Email += ''
         
         newCustomer = {'CustomerId':lastCID,
                        'FirstName':FirstName,
                        'LastName':LastName,
+                       'Company':Company,
                        'Address':Address,
                        'City':City,
+                       'State':State,
                        'Country':Country,
                        'PostalCode':PostalCode,
                        'Phone':Phone,
+                       'Fax':Fax,
                        'Email':Email,
                        'Username':Username,
                        'password':password,
-                       'SupportRepId':SupportRedId,
+                       'SupportRepId':supportRepId,
                        'rol':rol}
-        #cur.execute("""INSERT INTO Customer (CustomerId, FirstName, LastName, Address, City, Country, PostalCode, Phone, Email, Username, password, SupportRepId, rol)
-                    #VALUES ()
-                #)
-        print("User registered succesfully!")
+        cur.execute("""INSERT INTO Customer (CustomerId,FirstName, LastName, Company, Address, City, State, Country, PostalCode, Phone, Fax, Email, Username, password, SupportRepId, rol)
+                    VALUES (%(CustomerId)i,
+                    %(FirstName)s,
+                    %0(LastName)s,
+                    %(Company)s,
+                    %(Address)s,
+                    %(City)s,
+                    %(State)s,
+                    %(Country)s,
+                    %(PostalCode)s,
+                    %(Phone)s,
+                    %(Fax)s,
+                    %(Email)s,
+                    %(Username)s,
+                    %(password)s,
+                    %(SupportRepId)i,
+                    %(rol)i)""",newCustomer)
+        print("\n->User registered succesfully!")
     except:
-        print("Registration failed!")
+        print("\n->Registration failed!")
 
 def loginUser(con):
     try:
@@ -60,8 +104,8 @@ def loginUser(con):
         for r in admins:
             if user == r[0]:
                 currentUser['name'] = user
-                for name, us in currentUser.items():
-                    print(name + ": " + us)
+                currentUser['type'] = 'admin' 
+                adminView(currentUser)
                 
         cur.execute("""SELECT Username
                         FROM Customer""")
@@ -69,106 +113,121 @@ def loginUser(con):
         for r in customers:
             if user == r[0]:
                 userCredentials = {'username':user}
+                currentUser['name'] = user
                 cur.execute("""SELECT rol
                                 FROM Customer
                                 WHERE Username = %(username)s""",userCredentials)
                 rol = cur.fetchall()
                 for r in rol:
-                    print(user + " " + str(r[0]))
+                    userType = r[0]
+                    
+                if userType == 1:
+                    currentUser['type'] = 'Tier 1'
+                elif userType == 2:
+                    currentUser['type'] = 'Tier 2'
+                elif userType == 3:
+                    currentUser['type'] = 'Tier 3'
+                else:
+                    currentUser['type'] = 'Custom'
     except:
         print("Login Failed")
 
 def registerView():
-    global newuserEntry, newpasswordEntry, newFirstNameEntry, newLastNameEntry, newAddressEntry, newCityEntry, newCountryEntry, newPostalEntry, newPhoneEntry, newEmailEntry
+    global newuserEntry, newpasswordEntry, newFaxEntry, newFirstNameEntry, newLastNameEntry, newAddressEntry, newStateEntry, newCityEntry, newCountryEntry, newPostalEntry, newPhoneEntry, newEmailEntry, newCompanyEntry
     registerWindow = tkinter.Tk()
-    registerWindow.geometry("750x1000")
+    for i in range(7):
+        registerWindow.columnconfigure(i,weight=1)
+    registerWindow.geometry("850x950")
     registerWindow.title("Register Page")
-    space00 = tkinter.Label(registerWindow, text="")
-    space00.pack()
-    title = tkinter.Label(registerWindow, text="REGISTER")
-    title.config(font=("Steamer", 22, "bold"))
-    title.pack()
-    space0 = tkinter.Label(registerWindow, text="")
-    space0.pack()
+    space00 = tkinter.Label(registerWindow, text=" ").grid(row=0,column=5)
+    titleReg = tkinter.Label(registerWindow, text="REGISTER")
+    titleReg.grid(row=2,column=3)
+    titleReg.config(font=("Steamer",40,"bold"))
+    space0 = tkinter.Label(registerWindow, text=" ").grid(row=4,column=3)
     Instructions = tkinter.Label(registerWindow, text="Username")
-    Instructions.config(font=("Helvetica", 11))
-    Instructions.pack()
+    Instructions.grid(row=6,column=2)
+    Instructions.config(font=("Helvetica", 18))
     newuserEntry = tkinter.Entry(registerWindow, font="Helvetica 11")
-    newuserEntry.pack()
+    newuserEntry.grid(row=7,column=2)
     space1 = tkinter.Label(registerWindow, text="")
-    space1.pack()
     Instructions2 = tkinter.Label(registerWindow, text="Password")
-    Instructions2.config(font=("Helvetica", 11))
-    Instructions2.pack()
+    Instructions2.grid(row=6,column=4)
+    Instructions2.config(font=("Helvetica", 18))
     newpasswordEntry = tkinter.Entry(registerWindow, font="Helvetica 11")
-    newpasswordEntry.pack()
-    space2 = tkinter.Label(registerWindow, text="")
-    space2.pack()
+    newpasswordEntry.grid(row=7,column=4)
+    space2 = tkinter.Label(registerWindow, text="").grid(row=8,column=2)
     Instructions3 = tkinter.Label(registerWindow, text="First Name")
-    Instructions3.config(font=("Helvetica", 11))
-    Instructions3.pack()
+    Instructions3.grid(row=9,column=2)
+    Instructions3.config(font=("Helvetica", 18))
     newFirstNameEntry = tkinter.Entry(registerWindow, font="Helvetica 11")
-    newFirstNameEntry.pack()
+    newFirstNameEntry.grid(row=10,column=2)
     space3 = tkinter.Label(registerWindow, text="")
-    space3.pack()
     Instructions4 = tkinter.Label(registerWindow, text="Last Name")
-    Instructions4.config(font=("Helvetica", 11))
-    Instructions4.pack()
+    Instructions4.grid(row=9,column=4)
+    Instructions4.config(font=("Helvetica", 18))
     newLastNameEntry = tkinter.Entry(registerWindow, font="Helvetica 11")
-    newLastNameEntry.pack()
-    space4 = tkinter.Label(registerWindow, text="")
-    space4.pack()
+    newLastNameEntry.grid(row=10,column=4)
+    space4 = tkinter.Label(registerWindow, text="").grid(row=11,column=2)
     Instructions5 = tkinter.Label(registerWindow, text="Address")
-    Instructions5.config(font=("Helvetica", 11))
-    Instructions5.pack()
+    Instructions5.grid(row=12,column=2)
+    Instructions5.config(font=("Helvetica", 18))
     newAddressEntry = tkinter.Entry(registerWindow, font="Helvetica 11")
-    newAddressEntry.pack()
+    newAddressEntry.grid(row=13,column=2)
     space5 = tkinter.Label(registerWindow, text="")
-    space5.pack()
     Instructions6 = tkinter.Label(registerWindow, text="City")
-    Instructions6.config(font=("Helvetica", 11))
-    Instructions6.pack()
+    Instructions6.grid(row=12,column=4)
+    Instructions6.config(font=("Helvetica", 18))
     newCityEntry = tkinter.Entry(registerWindow, font="Helvetica 11")
-    newCityEntry.pack()
-    space6 = tkinter.Label(registerWindow, text="")
-    space6.pack()
+    newCityEntry.grid(row=13,column=4)
+    space6 = tkinter.Label(registerWindow, text="").grid(row=14,column=2)
     Instructions7 = tkinter.Label(registerWindow, text="Country")
-    Instructions7.config(font=("Helvetica", 11))
-    Instructions7.pack()
+    Instructions7.grid(row=15,column=2)
+    Instructions7.config(font=("Helvetica", 18))
     newCountryEntry = tkinter.Entry(registerWindow, font="Helvetica 11")
-    newCountryEntry.pack()
-    space7 = tkinter.Label(registerWindow, text="")
-    space7.pack()
+    newCountryEntry.grid(row=16,column=2)
+    Instructions12 = tkinter.Label(registerWindow, text="State")
+    Instructions12.grid(row=15,column=4)
+    Instructions12.config(font=("Helvetica", 18))
+    newStateEntry = tkinter.Entry(registerWindow, font="Helvetica 11")
+    newStateEntry.grid(row=16,column=4)
+    space7 = tkinter.Label(registerWindow, text="").grid(row=17,column=2)
     Instructions8 = tkinter.Label(registerWindow, text="Postal Code")
-    Instructions8.config(font=("Helvetica", 11))
-    Instructions8.pack()
+    Instructions8.grid(row=18,column=2)
+    Instructions8.config(font=("Helvetica", 18))
     newPostalEntry = tkinter.Entry(registerWindow, font="Helvetica 11")
-    newPostalEntry.pack()
+    newPostalEntry.grid(row=19,column=2)
     space8 = tkinter.Label(registerWindow, text="")
-    space8.pack()
     Instructions9 = tkinter.Label(registerWindow, text="Phone")
-    Instructions9.config(font=("Helvetica", 11))
-    Instructions9.pack()
+    Instructions9.grid(row=18,column=4)
+    Instructions9.config(font=("Helvetica", 18))
     newPhoneEntry = tkinter.Entry(registerWindow, font="Helvetica 11")
-    newPhoneEntry.pack()
-    space9 = tkinter.Label(registerWindow, text="")
-    space9.pack()
+    newPhoneEntry.grid(row=19,column=4)
+    space9 = tkinter.Label(registerWindow, text="").grid(row=20,column=20)
     Instructions10 = tkinter.Label(registerWindow, text="Email")
-    Instructions10.config(font=("Helvetica", 11))
-    Instructions10.pack()
+    Instructions10.grid(row=21,column=2)
+    Instructions10.config(font=("Helvetica", 18))
     newEmailEntry = tkinter.Entry(registerWindow, font="Helvetica 11")
-    newEmailEntry.pack()
-    space10 = tkinter.Label(registerWindow, text="")
-    space10.pack()
+    newEmailEntry.grid(row=22,column=2)
+    space10 = tkinter.Label(registerWindow, text="").grid(row=23,column=2)
+    Instructions11 = tkinter.Label(registerWindow, text="Company")
+    Instructions11.grid(row=21,column=4)
+    Instructions11.config(font=("Helvetica", 18))
+    newCompanyEntry = tkinter.Entry(registerWindow, font="Helvetica 11")
+    newCompanyEntry.grid(row=22,column=4)
+    Instructions12 = tkinter.Label(registerWindow, text="Fax")
+    Instructions12.grid(row=24,column=2)
+    Instructions12.config(font=("Helvetica", 18))
+    newFaxEntry = tkinter.Entry(registerWindow, font="Helvetica 11")
+    newFaxEntry.grid(row=25,column=2)
+    space10 = tkinter.Label(registerWindow, text="").grid(row=26,column=2)
     loginBtn = tkinter.Button(registerWindow, text="REGISTER", padx=15, pady=5, command = lambda: registerUser(con))
-    loginBtn.pack()
-    space11 = tkinter.Label(registerWindow, text="")
-    space11.pack()
-    loginBtn = tkinter.Button(registerWindow, text="BACK", padx=15, pady=5, command = lambda: registerUser(con))
-    loginBtn.pack()
+    loginBtn.grid(row=27,column=3)
+    space11 = tkinter.Label(registerWindow, text="").grid(row=28,column=3)
+    backBtn = tkinter.Button(registerWindow, text="BACK", padx=15, pady=5, command = lambda: loginView(con))
+    backBtn.grid(row=29,column=3)
     registerWindow.mainloop()
 
-def loginView():
+def loginView(con):
     global userEntry, passwordEntry
     window = tkinter.Tk()
     window.geometry("750x800")
@@ -218,16 +277,68 @@ def loginView():
     loginBtn.pack()
     window.mainloop()
 
-def adminView():
+def adminView(currentUser):
     adminWindow = tkinter.Tk()
     adminWindow.geometry("1350x800")
     adminWindow.title("Admin View")
-    space00 = tkinter.Label(window, text="")
+    space00 = tkinter.Label(adminWindow, text="")
     space00.pack()
+    showUser = tkinter.Label(adminWindow, text="Welcome " + currentUser['name'].title() + "!")
+    showUser.config(font=("Helvetica", 26, "bold"))
+    showUser.pack()
+    showUserType = tkinter.Label(adminWindow, text="User type: " + currentUser['type'])
+    showUserType.config(font=("Helvetica", 20, "bold"))
+    showUserType.pack()
+    space0 = tkinter.Label(adminWindow, text="")
+    space0.pack()
+    space1 = tkinter.Label(adminWindow, text="")
+    space1.pack()
+    registerArtist = tkinter.Button(adminWindow, text="Register New Artist", width=20, height=1)
+    registerArtist.pack()
+    registerAlbum = tkinter.Button(adminWindow, text="Register New Album", width=20, height=1)
+    registerAlbum.pack()
+    registerSong = tkinter.Button(adminWindow, text="Register New Song", width=20, height=1)
+    registerSong.pack()
+    modifyArtist = tkinter.Button(adminWindow, text="Modify Artist", width=20, height=1)
+    modifyArtist.pack()
+    modifyAlbum = tkinter.Button(adminWindow, text="Modify Album", width=20, height=1)
+    modifyAlbum.pack()
+    modifySong = tkinter.Button(adminWindow, text="Modify Song", width=20, height=1)
+    modifySong.pack()
+    removeArtist = tkinter.Button(adminWindow, text="Remove Artist", width=20, height=1)
+    removeArtist.pack()
+    removeAlbum = tkinter.Button(adminWindow, text="Remove Album", width=20, height=1)
+    removeAlbum.pack()
+    removeSong = tkinter.Button(adminWindow, text="Remove Song", width=20, height=1)
+    removeSong.pack()
+    deactivateSong = tkinter.Button(adminWindow, text="Deactivate Song", width=20, height=1)
+    deactivateSong.pack()
+    activateSong = tkinter.Button(adminWindow, text="Activate Song", width=20, height=1)
+    activateSong.pack()
+    manageUsers = tkinter.Button(adminWindow, text="Change User permission", width=20, height=1)
+    manageUsers.pack()
+    space2 = tkinter.Label(adminWindow, text="")
+    space2.pack()
+    space3 = tkinter.Label(adminWindow, text="")
+    space3.pack()
+    logoutBtn = tkinter.Button(adminWindow, text="LOGOUT", width=20, height=1, command = lambda: logout(adminWindow,currentUser))
+    logoutBtn.pack()
     adminWindow.mainloop()
+
+def newArtistView(currentUser):
+
+def newAlbumView(currentUser):
+
+def newSongView(currentUser):
+
+
+##################################################################################################################
+                                                #Programa
+##################################################################################################################
     
 try:
     global con
+    print("->Connecting to DB...")
     con = psycopg2.connect(
         host = "127.0.0.1",
         database = "proyecto1",
@@ -235,7 +346,8 @@ try:
         password = "uvg123",
         port = 5432
     )
-    print("Connected Succesfully to DB!")
-    loginView()
+    print("->Connected Succesfully to DB!")
+    loginView(con)
 except:
-    print("Connection to Database failed!")
+    print("->Connection to Database failed!")
+    print("->Check connection settings...")
